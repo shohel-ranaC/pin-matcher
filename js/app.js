e27fa0eb347a95a19b0f91a5427b92412f1d5dec
shohel-ranaC <akashshohel422@gmail.com>
2023-02-15 07:44:42 -0800

    fixed

--- js/app.js
+++ js/app.js
@@ -59,23 +59,23 @@ document.getElementById("verify-pin").addEventListener("click", () => {
 });
 
 // Quize question and answer start here
-let count = 0;
-console.log(parseInt("count" + 1));
-console.log(Math.pow(4, 0.5) + "spiderman" + "987" + Math.sqrt(9));
-function generatePin() {
-  return Math.floor(Math.random() * 90000) + 10000;
-}
-console.log(generatePin());
-console.log(!isNaN("10X"));
+// let count = 0;
+// console.log(parseInt("count" + 1));
+// console.log(Math.pow(4, 0.5) + "spiderman" + "987" + Math.sqrt(9));
+// function generatePin() {
+//   return Math.floor(Math.random() * 90000) + 10000;
+// }
+// console.log(generatePin());
+// console.log(!isNaN("10X"));
 
-const product = 5;
-const price = "7";
-const subTotal = product * price;
-const tax = subTotal / 10;
-const total = subTotal + tax;
-console.log(total);
+// const product = 5;
+// const price = "7";
+// const subTotal = product * price;
+// const tax = subTotal / 10;
+// const total = subTotal + tax;
+// console.log(total);
 
-const category = "pen";
-const performer = category + "-teller";
-console.log(performer);
+// const category = "pen";
+// const performer = category + "-teller";
+// console.log(performer);
 // Quize question and answer end here
